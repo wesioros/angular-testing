8f799523f438c0d8f8c838d3d9db24d771d064de
Yuri Haddad <yhaddad@outlook.com>
2021-09-19 23:01:58 +0200

    prueba

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,24 @@
-// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
+
   extends: [
     "eslint:recommended",
     "prettier",
     "plugin:@angular-eslint/recommended",
   ],
+
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
-    rootDir: "./src",
-    project: "tsconfig.app.json",
+    "sourceType": "module"
   },
-  plugins: [
+    plugins: [
     "@typescript-eslint",
     "prettier",
     "simple-import-sort",
     "unused-imports",
+    "html"
   ],
   rules: {
     semi: ["error", "always"],
